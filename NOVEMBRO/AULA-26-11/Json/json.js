cb192cdc29998dd1596096fe56e930fa039aa152
Kevin Stadnick <kevin_knoll@estudante.sesisenai.org.br>
2025-11-26 21:34:42 -0300

    Commit

---
+++ NOVEMBRO/AULA-26-11/Json/json.js
@@ -0,0 +1,33 @@
+var i = 0
+var chochorro = []
+var teste
+fetch("Json/chochorros.json")
+.then(res =>res.json())
+.then(data =>{
+    chochorro = data
+    teste(chochorro)
+    })
+    function teste(lista){
+         const container = document.getElementById("testesec");
+    container.innerHTML = "";
+
+    lista.forEach((cachorro, index) => {
+        const card = document.createElement("div");
+        card.className = "Movie-card";
+        card.innerHTML = `
+        <h1>${cachorro.nome}</h1>
+        <p><b>Raça - </b>${cachorro.nome}</p>
+        <p><b>Idade - </b>${cachorro.idade}</p>
+        <p><b>Porte - </b>${cachorro.porte}</p>
+        <p><b>Sexo - </b>${cachorro.sexo}</p>`;
+    container.appendChild(card);
+    });
+
+    }
+   
+    
+
+    // <p><b>Raça - </b>`+data.cachorro[i].raca+`</p>
+    // <p><b>Idade - </b>`+data.cachorro[i].idade+`</p>
+    // <p><b>Porte - </b>`+data.cachorro[i].porte+`</p>
+    // <p><b>Sexo - </b>`+data.cachorro[i].sexo+`</p>
